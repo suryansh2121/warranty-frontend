25ca50887f8a169563ae0cd273abafa050a9703d
suryansh2121 <rajputsuryansh491@gmail.com>
2025-08-03 17:29:47 +0530

    back to home button added in forgot password page

--- src/pages/ForgotPassword.jsx
+++ src/pages/ForgotPassword.jsx
@@ -2,40 +2,48 @@ import { useState } from "react";
 import { toast } from "react-toastify";
 import api from "../services/api";
 import { div } from "framer-motion/client";
+import { Link } from "react-router-dom";
 
-
-function ForgotPassword(){
-    const [email, setEmail] = useState("");
-    const handleSubmit = async(e) => {
-        e.preventDefault();
-        try{
-            await api.post("/api/auth/forgot-password", {email});
-            toast.success("Reset Link Sent to your email");
-            setEmail("");
-        }catch(err){
-            toast.error(err.response?.data?.message || "Failed to sent reset Link");
-        }
-    };
-    return(
-        <div className=" flex justify-center items-center min-h-screen bg-gradient-to-br from-indigo-950 via-purple-900 to-pink-900 text-white p-4">
-            <form onSubmit={handleSubmit} className="w-full max-w-md bg-white/10 p-8 rounded-xl shadow-xl backdrop-blur-md border border-white/20">
-            <h2 className="text-2xl font-bold mb-4">Forgot Password</h2>
-            <p>Enter your registered email</p>
-            <input type="email"
-            required 
-            placeholder= "Your Email"
-            className="w-full p-3 rounded-lg bg-white/20 text-white mb-4 placeholder-white/50"
-            value={email}
-            onChange={(e)=>setEmail(e.target.value)}
-            />
-          <button className="w-full bg-yellow-500 hover:bg-yellow-600 text-black font-bold py-2 rounded-lg transition">
-            Send Reset Link
-          </button>
-            </form>
-            
-        </div>
-    )
-
+function ForgotPassword() {
+  const [email, setEmail] = useState("");
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      await api.post("/api/auth/forgot-password", { email });
+      toast.success("Reset Link Sent to your email");
+      setEmail("");
+    } catch (err) {
+      toast.error(err.response?.data?.message || "Failed to sent reset Link");
+    }
+  };
+  return (
+    <div className=" flex justify-center items-center min-h-screen bg-gradient-to-br from-indigo-950 via-purple-900 to-pink-900 text-white p-4">
+      <form
+        onSubmit={handleSubmit}
+        className="w-full max-w-md bg-white/10 p-8 rounded-xl shadow-xl backdrop-blur-md border border-white/20"
+      >
+        <Link
+          to="/"
+          className="inline-block mb-4 px-4 py-1.5 rounded-full text-sm font-medium bg-white/10 text-white hover:bg-white/20 transition-colors shadow-md"
+        >
+          Back
+        </Link>
+        <h2 className="text-2xl font-bold mb-4">Forgot Password</h2>
+        <p>Enter your registered email</p>
+        <input
+          type="email"
+          required
+          placeholder="Your Email"
+          className="w-full p-3 rounded-lg bg-white/20 text-white mb-4 placeholder-white/50"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <button className="w-full bg-yellow-500 hover:bg-yellow-600 text-black font-bold py-2 rounded-lg transition">
+          Send Reset Link
+        </button>
+      </form>
+    </div>
+  );
 }
 
 export default ForgotPassword;
